61c12fff006612905bb0b6c05a53c698fd38508d
Naseer <naseern.rashid@gmail.com>
2026-01-13 10:31:49 -0500

    fixing prisma constructor

--- src/install-kit/prisma.js
+++ src/install-kit/prisma.js
@@ -1,6 +1,6 @@
 // src/install-kit/prisma.js
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 module.exports = { prisma };
